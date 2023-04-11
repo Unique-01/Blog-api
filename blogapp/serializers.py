84aa6e34b22788146e584374cf7a81b199114c1a
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2023-04-11 16:08:15 +0100

    create api views and serializer using generics

--- blogapp/serializers.py
+++ blogapp/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import Post
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','username','first_name','last_name']
+
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.PrimaryKeyRelatedField(source='author.username',read_only=True)
     class Meta:
         model = Post
-        fields= ['title','content']
+        fields= ['id','author','title','content']
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Post
+from django.contrib.auth.models import User
+from .serializers import PostSerializer,UserSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Create your views here.
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+
+
