c02b038851b7f82d0accb2ad7f36be2fe1e7080a
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2023-04-13 13:05:07 +0100

    recreate serializer

--- blogapp/serializers.py
+++ blogapp/serializers.py
@@ -2,16 +2,17 @@ from rest_framework import serializers
 from .models import Post
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    posts = serializers.HyperlinkedRelatedField(view_name='post-detail',read_only=True,many=True)
-    class Meta:
-        model = User
-        fields = ['url','id','username','posts']
 
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
-    author_username = serializers.ReadOnlyField(source='author.username')
-    author = serializers.HyperlinkedIdentityField(view_name='user-detail')
+    
+    author = serializers.ReadOnlyField(source='author.username')
     class Meta:
         model = Post
-        fields= ['url','id','author','author_username','title','content']
+        fields= ['url','id','author','title','content','created_at','updated_at']
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    posts = PostSerializer(many=True,read_only=True)
+    class Meta:
+        model = User
+        fields = ['url','id','username','posts']
